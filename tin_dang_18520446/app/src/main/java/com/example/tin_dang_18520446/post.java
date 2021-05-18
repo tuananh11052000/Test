b1040e039c4b866998b957828e59f1bb3671b681
Tuan Anh <18520446@gm.uit.edu.vn>
2021-05-18 15:50:02 +0700

    Tuan anh add deadline

---
+++ tin_dang_18520446/app/src/main/java/com/example/tin_dang_18520446/post.java
@@ -0,0 +1,13 @@
+package com.example.tin_dang_18520446;
+
+public class post {
+    int id;
+    int image;
+    String name;
+
+    public post(int id, int image, String name){
+        this.id = id;
+        this.image = image;
+        this.name = name;
+    }
+}
