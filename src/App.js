944fa9da31335b66c00ccc885f1e88c4644bea2f
Tuan Anh <18520446@gm.uit.edu.vn>
2021-08-11 17:15:29 +0700

    'uit'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './pages/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src="https://scontent.fhph1-2.fna.fbcdn.net/v/t1.6435-9/128255896_879142432822712_8831914308402454047_n.jpg?_nc_cat=105&ccb=1-4&_nc_sid=174925&_nc_ohc=SNUsSj8l0ZMAX_7kEEx&_nc_ht=scontent.fhph1-2.fna&oh=46fbfe7288eee43902393448136370d7&oe=6138B638" className="App-logo" alt="logo" />
+    //     <img src="https://scontent.fhph1-2.fna.fbcdn.net/v/t1.6435-9/128255896_879142432822712_8831914308402454047_n.jpg?_nc_cat=105&ccb=1-4&_nc_sid=174925&_nc_ohc=SNUsSj8l0ZMAX_7kEEx&_nc_ht=scontent.fhph1-2.fna&oh=46fbfe7288eee43902393448136370d7&oe=6138B638" className="App-logo" alt="logo" />
+    //     <img src="https://scontent.fhph1-2.fna.fbcdn.net/v/t1.6435-9/128255896_879142432822712_8831914308402454047_n.jpg?_nc_cat=105&ccb=1-4&_nc_sid=174925&_nc_ohc=SNUsSj8l0ZMAX_7kEEx&_nc_ht=scontent.fhph1-2.fna&oh=46fbfe7288eee43902393448136370d7&oe=6138B638" className="App-logo" alt="logo" />
+    //   </header>
+    // </div>
+    <Router>
+      <div className="App">
+        <Route path="/" exact component={Home} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+import '../../src/App.css';
+import { Button } from 'reactstrap';
+
+function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src="https://scontent.fhph1-2.fna.fbcdn.net/v/t1.6435-9/128255896_879142432822712_8831914308402454047_n.jpg?_nc_cat=105&ccb=1-4&_nc_sid=174925&_nc_ohc=SNUsSj8l0ZMAX_7kEEx&_nc_ht=scontent.fhph1-2.fna&oh=46fbfe7288eee43902393448136370d7&oe=6138B638" className="App-logo" alt="logo" />
+                <img src="https://scontent.fhph1-2.fna.fbcdn.net/v/t1.6435-9/128255896_879142432822712_8831914308402454047_n.jpg?_nc_cat=105&ccb=1-4&_nc_sid=174925&_nc_ohc=SNUsSj8l0ZMAX_7kEEx&_nc_ht=scontent.fhph1-2.fna&oh=46fbfe7288eee43902393448136370d7&oe=6138B638" className="App-logo" alt="logo" />
+                <img src="https://scontent.fhph1-2.fna.fbcdn.net/v/t1.6435-9/128255896_879142432822712_8831914308402454047_n.jpg?_nc_cat=105&ccb=1-4&_nc_sid=174925&_nc_ohc=SNUsSj8l0ZMAX_7kEEx&_nc_ht=scontent.fhph1-2.fna&oh=46fbfe7288eee43902393448136370d7&oe=6138B638" className="App-logo" alt="logo" />
+            </header>
+            <Button color="success">tab here</Button>
+        </div>
+    );
+}
+
+export default Home;
